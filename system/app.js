00413e921f0f9cf2bfce984964c3af9561eb4200
Boehm Tomi S <tboehm@da1-cs-bk107-26.helsinki.fi>
2014-03-07 15:05:32 +0200

    json output

--- system/app.js
+++ system/app.js
@@ -43,12 +43,12 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(express.favicon());
 app.use(express.logger('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.multipart());
 app.use(express.methodOverride());
 app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
 if ('development' == app.get('env')) {
@@ -74,6 +74,18 @@ app.post('/file-upload',function(req, res){
   
   
 })
+
+app.get('/uploads.json',function(req,res){
+  
+  gfs.files.find().toArray(function (err, files) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(files);
+    }
+  });
+  
+});
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
